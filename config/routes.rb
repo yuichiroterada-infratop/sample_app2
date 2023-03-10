0218013989f31e8c9b6106ad236bde6539c573bd
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-10 02:44:15 +0000

    [Fin]4-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/lists_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class ListsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get lists_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get lists_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get lists_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get lists_edit_url
+    assert_response :success
+  end
+end
