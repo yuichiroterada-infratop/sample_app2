799f3f3b9926f8a37d6e1f42577ef56e5e1565af
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-10 12:11:01 +0000

    [Fin]4-2 投稿機能の追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new
+    list.save
+    redirect_to "/top"
   end
 
   def index
@@ -9,5 +16,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  post 'lists' => 'lists#create'
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
