256b0839b4ef43f55d4eec8a03e5258f085e142c
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-10 12:27:35 +0000

    [Fin]4-3 投稿一覧画面（indexアクション）の作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,6 +10,7 @@ class ListsController < ApplicationController
   end
 
   def index
+    @lists = List.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists/index'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
